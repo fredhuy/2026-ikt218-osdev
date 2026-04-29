382baf778e4ba63dc9d3f74977cd0106ad4ab937
David Alexander Danielsen <gruveexp123@gmail.com>
2026-04-29 14:00:46 +0000

    remove old kernel code

--- src/91_nineone/src/kernel.c
+++ src/91_nineone/src/kernel.c
@@ -3,10 +3,6 @@
 #define terminal_write print
 
 int main() {
-    char *video_memory = (char*) 0xB8000; // startadresse for VGA tekstmodus
-
-    const char *str = "Velkommen til DaviDOS!";
-    
     print("Velkommen til FreDDaviDOS!", COLOR(YELLOW, BLUE), 0, 0);
 
     while (1) { // coming soon
