1ac6faad51f7f69df79dc43fea89202b95b0856a
David Alexander Danielsen <gruveexp123@gmail.com>
2026-05-01 22:51:02 +0000

    add menu

---
+++ src/91_nineone/include/menu.h
@@ -0,0 +1,6 @@
+#ifndef MENU_H
+#define MENU_H
+
+void draw();
+
+#endif // MENU_H
--- src/91_nineone/src/kernel.c
+++ src/91_nineone/src/kernel.c
@@ -1,4 +1,5 @@
 #include "terminal.h"
+#include "menu.h"
 #include "colors.h"
 #include "libc/stdint.h"
 #include "kernel/memory.h"
@@ -10,7 +11,7 @@ extern uint32_t end;
 
 int main() {
     init_gdt();
-    terminal_write("Velkommen til FreDDaviDOS!", COLOR(YELLOW, BLUE), 0, 0);
+    terminal_write("Velkommen til FreDDaviDOS!", COLOR(YELLOW, BLUE), 1, 1);
 
 
     idt_init();
@@ -20,16 +21,13 @@ int main() {
     idt_enable_interrupts();
 
 
-    terminal_write("IDT loaded", COLOR(WHITE, BLACK), 0, 1);
+    terminal_write("IDT loaded", COLOR(WHITE, BLACK), 1, 2);
 
     asm volatile("int $0x3");
 
-    terminal_write("After interrupt", COLOR(WHITE, BLACK), 0, 3);
+    terminal_write("After interrupt", COLOR(WHITE, BLACK), 1, 3);
 
-    terminal_write("Keyboard input:", COLOR(WHITE, BLACK), 0, 9);
-
-    
-    terminal_write("Velkommen til FreDDaviDOS!", COLOR(YELLOW, BLUE), 0, 0);
+    terminal_write("Keyboard input:", COLOR(WHITE, BLACK), 1, 9);
     
     init_kernel_memory(&end);
 
@@ -40,6 +38,7 @@ int main() {
     void* some_memory = malloc(12345); 
     void* memory2 = malloc(54321); 
     void* memory3 = malloc(13331);
+    draw();
 
     while (1) { // coming soon 
         
